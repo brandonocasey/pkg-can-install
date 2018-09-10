6fe7a8f5c80968af06921e6669d2ecf939ccb92b
brandonocasey <brandonocasey@gmail.com>
2018-09-10 14:13:04 -0400

    feat: initial

---
+++ pkg-can-install.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+/* eslint-disable no-console */
+
+const path = require('path');
+const spawnSync = require('child_process').spawnSync;
+const crypto = require('crypto');
+const TEMP_DIR = require('os').tmpdir();
+const PKG_DIR = process.cwd();
+const randomString = crypto.randomBytes(20).toString('hex');
+const WORKING_DIR = path.join(TEMP_DIR, randomString);
+const pkg = require(path.join(PKG_DIR, 'package.json'));
+const shell = require('shelljs');
+
+// create the working dir
+shell.mkdir(WORKING_DIR);
+
+// get a list of all files to copy, minus node_modules
+const files = shell.ls('-A', PKG_DIR)
+  .filter((f) => !(/node_modules/).test(f))
+  .map((f) => path.join(process.cwd(), f));
+
+// copy all files to working_dir
+shell.cp('-R', files, WORKING_DIR);
+
+// try the install
+console.log(`pkg-can-install: Checking if ${pkg.name} will be installable after publish`);
+const result = spawnSync('npm', ['i', '--production'], {cwd: WORKING_DIR, stdio: 'inherit'});
+
+if (result.status === 0) {
+  console.log(`pkg-can-install: ${pkg.name} will be installable!`);
+} else {
+  console.error(`pkg-can-install: ${pkg.name} will not be installable!`);
+}
+
+// cleanup
+shell.rm('-rf', WORKING_DIR);
+
+process.exit(result.status);
